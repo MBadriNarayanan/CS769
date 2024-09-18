78f881991d17edd370b032527c4325d864d0cb9d
MBadriNarayanan <MBadriNarayanan>
2024-09-18 18:45:28 -0400

    [ UPDATE ] - Format Vocab

--- assignment1/vocab.py
+++ assignment1/vocab.py
@@ -8,11 +8,11 @@ class Vocab(object):
         self.id2word = dict()
         self.pad_id = self.unk_id = None
         if pad:
-            self.add('<pad>')
-            self.pad_id = self.word2id['<pad>']
+            self.add("<pad>")
+            self.pad_id = self.word2id["<pad>"]
         if unk:
-            self.add('<unk>')
-            self.unk_id = self.word2id['<unk>']
+            self.add("<unk>")
+            self.unk_id = self.word2id["<unk>"]
         self.max_size = max_size
 
     def __getitem__(self, word):
@@ -22,13 +22,13 @@ class Vocab(object):
         return word in self.word2id
 
     def __setitem__(self, key, value):
-        raise ValueError('vocabulary is readonly')
+        raise ValueError("vocabulary is readonly")
 
     def __len__(self):
         return len(self.word2id)
 
     def __repr__(self):
-        return 'Vocabulary[size=%d]' % len(self)
+        return "Vocabulary[size=%d]" % len(self)
 
     def id2word(self, wid):
         return self.id2word[wid]
@@ -51,7 +51,7 @@ class Vocab(object):
         for words in texts:
             for w in words:
                 word_freq[w] += 1
-        
+
         top_k_words = sorted(word_freq.keys(), reverse=True, key=word_freq.get)
         for word in top_k_words:
             if self.max_size:
